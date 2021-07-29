3ea6e3ba9cab6bb05d39ed3b1391367379866e18
vaivodeieva <ieva.webdevelopment@gmail.com>
2021-07-29 22:32:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-     
+        <Router>
+            <Header />
+            <Switch>
+
+                <Route exact path="/">
+                    <Home />
+                </Route>
+
+                <Route>
+                    <Page404 />
+                </Route>
+
+            </Switch>
+            <Footer />
+        </Router>
+
+        
     </div>
-  );
+);
 }
 
 export default App;
